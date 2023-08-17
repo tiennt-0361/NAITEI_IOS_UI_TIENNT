0cc1a121dfaeecf5fef29aad725a11455c0b3a65
ThanhTien3103 <thanhtien31032002@gmail.com>
2023-08-17 15:26:00 +0700

    add UI

--- AppMusic/ViewController.swift
+++ AppMusic/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setNeedsStatusBarAppearanceUpdate() // Gọi hàm này để cập nhật trạng thái thanh trạng thái
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent // Màu chữ và biểu tượng trạng thái sáng trên nền màn hình đen
+    }
+
+    override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        return .slide // Hiệu ứng khi cập nhật trạng thái thanh trạng thái
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+     
+       
+//        Do any additional setup after loading the view.
+       
+        
     }
 
 
